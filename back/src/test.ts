0ef4f9ac444ded623aecd6cf2f2e3465ae66f02a
janilson-123 <103967519+janilson-123@users.noreply.github.com>
2022-04-18 19:36:49 -0300

    Update test.ts

--- back/src/test.ts
+++ back/src/test.ts
@@ -1,9 +1,2 @@
 import { MyEnum } from '@test/shared/src/my-enum'
-
-function test(x: number){
-	return x == MyEnum.B
-}
-
-setTimeout(test, 1000, 1)
-setTimeout(test, 2000, 2)
-setTimeout(test, 3000, 3)
+console.log(2 == MyEnum.B)
